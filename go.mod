f5e04b8ce372305849decf4da8c21ac05976ed02
Nenad <nbursa@gmail.com>
2025-05-31 21:52:31 +0200

    chore: clear main branch to import sentience-rs code

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/nbursa/sentience
-
-go 1.24.1
--- internal/types/version.go
+++
@@ -1,7 +0,0 @@
-package types
-
-const (
-	Version     = "v0.1.0"
-	VersionName = "Sentience Language"
-	ReleaseDate = "2025-05-29"
-)
